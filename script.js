34619a2c886d3e7814f125b7009272590cfa4192
SaiRajath <cbsairajath@gmail.com>
2020-11-16 15:33:18 +0530

    Deep learning of JS

---
+++ script.js
@@ -0,0 +1,13 @@
+/*var sum = 0;
+console.log(sum);*/
+ var sum;
+ sum = 5
+// console.log(sum)
+
+ function add(x = 10, y =10){
+    var sum = x + y;
+    console.log(sum)
+    return sum;
+ }
+ 
+add(15,15)
